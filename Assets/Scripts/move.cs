85676f6538fbc71ed2d65f449e2554e70b8baa15
Riley Mahr <rwmahr@gmail.com>
2022-09-09 16:41:49 -0500

    Initial Commit
    Populated legacy scripts and got basic functionality working.

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	public int speedZ = 0;
+	public int speedX = 0;
+	private bool planetForce = true;
+
+
+	void Start () {
+		Vector3 initial = new Vector3 (speedX, 0, speedZ);
+		this.GetComponent<Rigidbody>().AddForce(initial);
+	}
+
+}
